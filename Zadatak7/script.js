68eacc5186edde93d307c0847561d4e3cf6f2740
teodora3991 <todorovic.t93@gmail.com>
2018-06-03 15:54:19 +0200

    Zadatak7

--- Zadatak7/script.js
+++ Zadatak7/script.js
@@ -3,10 +3,12 @@ function check () {
     var broj = parseInt(document.getElementById('br1').value);
     var message = '';
 
-    if (broj %2 === 0) {
-        message = 'Broj je paran.'
+    if (broj %2 === 0 && broj>100) {
+        message = 'Broj je paran i veci od 100.'
+    } else if (broj %2 !== 0 && broj < 100) {
+        message = 'Broj je neparan i manji od 100.'
     } else {
-        message = 'Broj je neparan.'
+        message = 'Nista od ta dva.'
     }
 
     document.getElementById('message').value = message;
