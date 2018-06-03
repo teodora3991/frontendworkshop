82107b95ce8c30ea8eea25698c6870783285638e
teodora3991 <todorovic.t93@gmail.com>
2018-06-03 12:32:06 +0200

    Zadatak3

---
+++ Zadatak3/script.js
@@ -0,0 +1,9 @@
+function zbir() {   
+            var Broj1 = document.getElementById('br1').value;
+            var Broj2 = document.getElementById('br2').value;
+
+            var Rezultat = parseInt(Broj1) + parseInt(Broj2);
+
+            document.getElementById("br3").value = Rezultat;
+
+            }
